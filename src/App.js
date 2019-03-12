04c82ad4a9727fee4406cb51fd37a211d5ed505a
Cheyenne <cheyenneholland@ymail.com>
2019-03-12 11:02:16 -0500

    created a react application, started creating the playlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+let textColor = "#fff";
+let defaultStyle={
+  color: textColor
+}
+class Aggregate extends Component {
+  render(){
+    return (
+      <div style={{...defaultStyle, width:"40%", display: 'inline-block'}}>
+        <h2 style ={defaultStyle}>Number Text</h2>
+      </div>
+    );
+  }
+}
+class Filter extends Component{
+  render(){
+    return(
+      <div style = {defaultStyle}>
+        <img/>
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+class Playlist extends Component{
+  render(){
+    return(
+      <div style ={{...defaultStyle, width: "25%", display:'inline-block'}}>
+        <img/>
+        <h3>Playlist name</h3>
+        <ul><li>song1</li><li>song2</li><li>song3</li></ul>
+      </div>
+    );
+  }
+}
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App-header">
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
     );
   }
